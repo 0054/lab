ad4f0793b9aa5a278408055f20ac8eebcbd69cf2
0054 <kofe54@gmail.com>
2021-03-02 23:16:08 +0300

    create tf backend

---
+++ cloud-lab/aws/infra/README.md
@@ -0,0 +1,6 @@
+# usage
+
+configure aws profile and export AWS_PROFILE, for example:
+```
+export AWS_PROFILE=lab
+```
--- cloud-lab/aws/infra/main.tf
+++ cloud-lab/aws/infra/main.tf
@@ -1,9 +1,12 @@
 terraform {
-  # backend "s3" {
-  #   bucket = "0054-tf-remote-state"
-  #   key    = "lab-state/terraform.tfstate"
-  #   region = "eu-west-1"
-  # }
+  backend "s3" {
+    bucket         = "0054-tf-remote-state"
+    key            = "lab/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
@@ -13,46 +16,40 @@ terraform {
 }
 
 provider "aws" {
-  profile = "lab-root"
+  profile = "lab"
   region  = "eu-west-1"
 
 }
 
-# resource "aws_instance" "example" {
-#   ami           = "ami-08d70e59c07c61a3a"
-#   instance_type = "t2.micro"
-# }
-
-
-# resource "aws_s3_bucket_policy" "terraform-state-lab-policy" {
-#   bucket = aws_s3_bucket.terraform-state.id
-
-#   # Terraform's "jsonencode" function converts a
-#   # Terraform expression's result to valid JSON syntax.
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Id      = "terraform-state-lab-policy"
-#     Statement = [
-#       {
-#         Effect   = "Allow"
-#         Action   = "s3:ListBucket"
-#         Resource = "arn:aws:s3:::${aws_s3_bucket.tf-remote-state.arn}"
-#       },
-#       {
-#         Effect   = "Allow"
-#         Action   = ["s3:GetObject", "s3:PutObject"],
-#         Resource = "arn:aws:s3:::${aws_s3_bucket.tf-remote-state.arn}/lab-state"
-#       }
-#     ]
-#   })
-# }
-
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  tags = {
+    Environment = "lab"
+  }
+}
 
 resource "aws_s3_bucket" "tf-remote-state" {
   bucket = "0054-tf-remote-state"
-  acl    = "private"
+  # acl    = "private"
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = {
-    Name        = "0054-tf-remote-state"
     Environment = "lab"
   }
 }
